dd8e17bf7181bb4769057efa8a1e5d0a5614c874
yhg0337 <58197935+yhg0337@users.noreply.github.com>
2020-08-06 19:09:14 +0900

    Updates

---
+++ precache-manifest.da5f756faceed2688e54356a059d91ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0afab819b39221c9810f6279ef382ce",
+    "url": "/git-book-list/index.html"
+  },
+  {
+    "revision": "56165bfe02cb48c5ce16",
+    "url": "/git-book-list/static/css/main.897322b6.chunk.css"
+  },
+  {
+    "revision": "28dc1e8caae392ed82c2",
+    "url": "/git-book-list/static/js/2.df96256c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/git-book-list/static/js/2.df96256c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56165bfe02cb48c5ce16",
+    "url": "/git-book-list/static/js/main.9c8cf552.chunk.js"
+  },
+  {
+    "revision": "fa3cf16755f443188e98",
+    "url": "/git-book-list/static/js/runtime-main.f0633fc5.js"
+  }
+]);
